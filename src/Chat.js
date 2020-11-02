30faae36d31c1b6d72bd61eba45d76dbc38d5008
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-11-02 16:31:52 +0000

    send message form and button created in Chat.js awaiting firebase intergration

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,13 @@ import React from 'react'
 import './Chat.css'
 
 function Chat() {
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        //Firebase
+    }
+
     return (
         <div className='chat'>
             {/* chat header  */}
@@ -10,6 +17,12 @@ function Chat() {
                 <strong>Details</strong>
             </div>
             {/* chat messages */}
+            <div className="chat_input">
+                <form>
+                    <input placeholder='iMessage' type='text'/>
+                    <button onClick={sendMessage} >Send Message</button>
+                </form>
+            </div>
 
             {/* chat input */}
             

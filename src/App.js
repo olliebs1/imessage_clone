fd006d98ddcaa228d0f5174ce0b4753bfccb166f
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-11-03 15:18:27 +0000

    query added in App.js to see if user is logged in - asks them to if they arent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Imessage from './Imessage'
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-        <Imessage />
+      {user ? <Imessage /> : <h2>You need to login</h2>}
     </div>
   );
 }

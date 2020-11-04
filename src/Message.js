17ff14dc6b7667343f84d7c7c36940b819baa743
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-11-04 20:48:09 +0000

    direct condition added to message div to check if user email = email that sends message - styling added for sent and recieved messages

--- src/Message.js
+++ src/Message.js
@@ -1,14 +1,19 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from './features/userSlice'
 import './Message.css'
 
 function Message({
     id, 
     contents: { timestamp, displayName, email, message, photo, uid },
 }) {
+
+    const user = useSelector(selectUser)
+
     return (
-        <div className='message'>
-            <Avatar src={photo}/>
+        <div className={`message ${user.email === email && "message_sender"}`}>
+            <Avatar className='message_photo' src={photo}/>
             <p>{message}</p>
             <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
         </div>
